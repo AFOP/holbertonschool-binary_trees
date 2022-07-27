7f27951a2cf77457b18ccfd90637403dd3a4111b
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-07-27 13:32:58 -0500

    taks 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,25 +10,21 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = NULL, *actual_node = NULL;
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+		return (NULL);
 
 	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
 
-	if (parent->left)
+	if (parent->left != NULL)
 	{
-		actual_node = parent->left;
-		parent->left = new_node;
-		new_node->left = actual_node;
-		actual_node->parent = new_node;
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
 	}
-	else
-	{
-		parent->left = new_node;
-	}
+	parent->left = new_node;
 
 	return (new_node);
 }
