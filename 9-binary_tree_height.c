397154830aa050cb046062506414cd4105095c4c
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-07-27 19:07:28 -0500

    taks 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,31 +1,30 @@
 #include "binary_trees.h"
 /**
- * max - funtion max between a and b
- * 
- * @a: variable 
+ * max -binary_tree_preorder
+ * @a: variable
  * @b: variable
  * @return height
  */
 int max(int a, int b)
 {
-        if (a >= b)
-                return (a);
-        return (b);
+	if (a >= b)
+		return (a);
+	return (b);
 }
 /**
  * height -binary_tree_preorder
- *
  * @tree: tree
  * @func: func
+ * @max: max
  */
 size_t height(const binary_tree_t *tree)
 {
-        if (tree == NULL)
-                return (-1);
-        return (max(height(tree->left), height(tree->right)) + 1);
+	if (tree == NULL)
+		return (-1);
+	return (max(height(tree->left), height(tree->right)) + 1);
 }
 /**
- * height -binary_tree_preorder
+ * binary_tree_height -binary_tree_preorder
  *
  * @tree: tree
  * @func: func
