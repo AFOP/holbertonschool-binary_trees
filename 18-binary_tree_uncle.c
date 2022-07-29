a109d74ab0dd8adbc6aebd4bf65605b4a3303406
AFOP <4592@holbertonschool.com>
2022-07-28 23:24:29 -0500

    Task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -5,7 +5,7 @@
 /**
  * binary_tree_uncle - function that finds the uncle of a node
  * @node: is a pointer to the node to find the uncle
- * Return: node uncle or null in case of error.
+ * Return: node uncle or null in case of error
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
@@ -16,6 +16,6 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node->parent->parent == NULL)
 		return (NULL);
 	if (node->parent->parent->left == node->parent)
-		return (node->parent->parent->right);
-	return (node->parent->parent->left);
+		return (node->parent->parent->left);
+	return (node->parent->parent->right);
 }
